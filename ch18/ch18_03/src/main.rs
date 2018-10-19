b8b1e2bfdebc2d79283f7b37f931a98e435804b3
kwon <kkdosk@naver.com>
2018-10-19 19:41:47 +0900

    add

--- ch18/ch18_03/src/main.rs
+++ ch18/ch18_03/src/main.rs
@@ -123,6 +123,141 @@ fn main() {
 			.sum();
 
 		println!("sum of squares: {}", sum_of_squares);
+
+
+		let ((feet, inches), Point {x, y}) = ((3, 10), Point {x: 3, y: -10});
+		println!("{} {} {} {}", feet, inches, x, y);
+
+		foo(3, 4);
+	}
+
+	{
+		let mut setting_value = Some(5);
+		let new_setting_value = Some(10);
+
+		match (setting_value, new_setting_value) {
+			(Some(_), Some(_)) => {
+				println!("Can't overwrite an existing customized value");
+			}
+			_ => {
+				setting_value = new_setting_value;
+			}
+		}
+
+		println!("setting is {:?}", setting_value);
+
+		let numbers = (2, 4, 8, 16, 32);
+
+		match numbers {
+			(first, _, third, _, fifth) => {
+				println!("Some numbers: {}, {}, {}", first, third, fifth)
+			},
+		}
+
+		let _x = 5;
+		let y = 10;
+
+		let s = Some(String::from("Hello!"));
+
+		if let Some(_s) = s {
+			println!("found a string!");
+		}
+
+		// println!("{:?}", s);
+	}
+
+	{
+		let s = Some(String::from("Hello!"));
+
+		if let Some(_) = s {
+			println!("found a string");
+		}
+
+		println!("{:?}", s);
+
+		struct Point {
+			x: i32,
+			y: i32,
+			z: i32,
+		}
+
+		let origin = Point { x: 0, y: 0, z: 0 };
+		match origin {
+			Point {x, .. } => println!("x is {}", x),
+		}
+
+		let numbers = (2, 4, 8, 16, 32);
+
+		match numbers {
+			(first, .. , last) => {
+				println!("Some numbers: {}, {}", first, last);
+			},
+		}
+
+		// match numbers {
+		// 	(.., second, ..) => {
+		// 		println!("Some numbers: {}", second);
+		// 	},
+		// }
+
+		let num = Some(10);
+		match num {
+			Some(x) if x < 5 => println!("less than five: {}", x),
+			Some(x) => println!("{}", x),
+			None => (),
+		}
+	}
+
+	{
+		let x = Some(5);
+		let y = 10;
+
+		match x {
+			Some(50) => println!("Got 50"),
+			Some(n) if n == y => println!("Matched, n = {:?}", n),
+			_ => println!("Default case, x = {:?}", x),
+		}
+
+		println!("at the end: x = {:?}, y = {:?}", x, y);
+	}
+
+	{
+		let x = 4;
+		let y = false;
+
+		match x {
+			4 | 5 | 6 if y => println!("yes"),
+			_ => println!("no")
+		}
+	}
+
+	{
+		enum Message {
+			Hello { id: i32 },
+		}
+
+		let msg = Message::Hello { id: 5 };
+
+		match msg {
+			Message::Hello { id: id_variable @ 3...7 } => {
+				println!("Found an id in range: {}", id_variable)
+			},
+			Message::Hello { id: 10...12 } => {
+				println!("Found an id in another range");
+			},
+			Message::Hello { id } => {
+				println!("Found some other id: {}", id)
+			},
+		}
+
+		let robot_name = &Some(String::from("Bors"));
+
+		match robot_name {
+			&Some(ref name) => println!("Found a name: {}", name),
+			None => (),
+		}
+
+		println!("robot name is: {:?}", robot_name);
 	}
 }
 
@@ -148,4 +283,8 @@ enum MessageColor {
 	Move { x: i32, y: i32 },
 	Write(String),
 	ChangeColor(Color),
+}
+
+fn foo(_: i32, y: i32) {
+	println!("This code only uses the y parameter: {}", y);
 }
