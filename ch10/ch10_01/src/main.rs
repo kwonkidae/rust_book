0d78c23805dd26ff7878d1b7e34b1ce3477f1bf3
kwon <kkdosk@naver.com>
2018-10-25 19:33:25 +0900

    a

---
+++ ch10/ch10_01/src/main.rs
@@ -0,0 +1,42 @@
+
+
+
+fn main() {
+	println!("Hello, world!");
+}
+
+fn largest_i32(list: &[i32]) -> i32 {
+	let mut largest = list[0];
+
+	for &item in list.iter() {
+		if item > largest {
+			largest = item;
+		}
+	}
+	largest
+}
+
+fn largest_char(list: &[char]) -> char {
+	let mut largest = list[0];
+
+	for &item in list.iter() {
+		if item > largest {
+			largest = item;
+		}
+	}
+
+	largest
+}
+
+// fn largest<T>(list: &[T]) -> T
+// {
+// 	let mut largest = list[0];
+
+// 	for &item in list.iter() {
+// 		if item > largest {
+// 			largest = item;
+// 		}
+// 	}
+
+// 	largest
+// }
