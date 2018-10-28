c2284ae4c4707c30d1ec09a05759cb089db564c8
kwon <kkdosk@naver.com>
2018-10-28 23:22:11 +0900

    a

--- ch10/ch10_01/src/main.rs
+++ ch10/ch10_01/src/main.rs
@@ -2,7 +2,68 @@
 
 
 fn main() {
-	println!("Hello, world!");
+	let number_list = vec![34, 50, 25, 100 ,65];
+
+	let result = largest_i32(&number_list);
+	println!("The largest number is {}", result);
+
+	let char_list = vec!['y', 'm', 'a', 'q'];
+
+	let result = largest_char(&char_list);
+	println!("The largest char is {}", result);
+
+	{
+		let integer = Point { x: 5, y: 10 };
+		let float = Point { x: 1.0, y: 4.0 };
+
+		// let wont_work = Point { x: 5, y: 4.0 };
+	}
+
+	{
+		struct Point<T, U> {
+			x: T,
+			y: U,
+		}
+
+		impl<T, U> Point<T,U> {
+			fn mixup<V,W>(self, other: Point<V, W>) -> Point<T, W> {
+				Point {
+					x: self.x,
+					y: other.y,
+				}
+			}
+		}
+
+		let both_integer = Point { x: 5, y: 10 };
+		let both_float = Point { x: 1.0, y: 4.0 };
+		let integer_and_float = Point { x: 5, y: 4.0 };
+
+		let p1 = Point { x: 5, y: 10.4 };
+		let p2 = Point { x: "Hello", y: 'c' };
+
+		let p3 = p1.mixup(p2);
+		println!("p3.x = {}, p3.y = {}", p3.x, p3.y);
+	}
+
+	{
+		enum Option<T> {
+			Some(T),
+			None,
+		}
+
+		enum Result<T, E> {
+			Ok(T),
+			Err(E),
+		}	
+	}
+
+	{
+		let p = Point { x: 5, y: 10 };
+		println!("p.x = {}", p.x());
+
+		let float_p = Point { x: 5.5, y: 10.5 };
+		println!("p.origin: {}", float_p.distance_from_origin());
+	}
 }
 
 fn largest_i32(list: &[i32]) -> i32 {
@@ -26,6 +87,23 @@ fn largest_char(list: &[char]) -> char {
 	}
 
 	largest
+}
+
+struct Point<T> {
+	x: T,
+	y: T,
+}
+
+impl<T> Point<T> {
+	fn x(&self) -> &T {
+		&self.x
+	}
+}
+
+impl Point<f32> {
+	fn distance_from_origin(&self) -> f32 {
+		(self.x.powi(2) + self.y.powi(2)).sqrt()
+	}
 }
 
 // fn largest<T>(list: &[T]) -> T
